f4e38de0353359d97de01b928612ba1b62f11378
starsenea <rishipadhye@gmail.com>
2023-08-10 14:01:44 +0400

    IP address added

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,16 +3,20 @@ import React from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import IPAddress from './IPAddress';
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Banner />
-      <Exhibit title="IP Metrics">
-        
+      <Exhibit title="Public IPv4 Address">
+        <IPAddress url='https://api.ipify.org?format=json' />
+      </Exhibit>
+      <Exhibit title="Public IPv6 Address">
+        <IPAddress url='https://api64.ipify.org?format=json' />
       </Exhibit>
       <Exhibit title="Latency Metrics">
-        
+      
       </Exhibit>
     </div>
   );
